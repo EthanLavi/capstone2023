15e7b6f858435f30a6d9068d0918a6ba3a7f7063
EthanLavi <89608076+EthanLavi@users.noreply.github.com>
2023-10-24 16:51:52 -0400

    Update night_time_label.py
    last edits for sure this time...

--- labeling/nighttime/night_time_label.py
+++ labeling/nighttime/night_time_label.py
@@ -14,6 +14,8 @@ f.close()
 # Get data per image
 data_it = data.split("\n")
 for img_num, img_label in enumerate(data_it):
+    if img_num % 100 == 0:
+        print(img_num) # progress indication
     label_name = f"img_{img_num}.txt"
     img_name = f"img_{img_num}.jpg"
 
@@ -23,9 +25,11 @@ for img_num, img_label in enumerate(data_it):
     assert(int(tokens[0]) == img_num)
     lines = ""
     if not os.path.isfile(os.path.join(folder_with_imgs, img_name)):
-        continue
-    img_content = cv.imread(os.path.join(folder_with_imgs, img_name))
-    h, w, c = img_content.shape
+        h, w = 1024, 1280
+    else:
+        img_content = cv.imread(os.path.join(folder_with_imgs, img_name))
+        h, w, c = img_content.shape
+    assert(h == 1024 and w == 1280)
     for img_i in range(int(tokens[1])):
         i = 2 + (img_i * 4)
         x1, y1, width, height = (float(tokens[i]), float(tokens[i + 1]), float(tokens[i + 2]), float(tokens[i + 3]))
